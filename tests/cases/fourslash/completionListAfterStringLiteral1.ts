934a7b2ec00fa4e8fa3f25503485ed414455efa6
Andy Hanson <anhans@microsoft.com>
2018-06-05 09:00:33 -0700

    wip

--- tests/cases/fourslash/completionListAfterStringLiteral1.ts
+++ tests/cases/fourslash/completionListAfterStringLiteral1.ts
@@ -2,6 +2,12 @@
 
 ////"a"./**/
 
-goTo.marker();
-verify.not.completionListContains('alert');
-verify.completionListContains('charAt');
+verify.completions({
+    marker: "",
+    exact: [
+        "toString", "charAt", "charCodeAt", "concat", "indexOf", "lastIndexOf", "localeCompare", "match", "replace", "search", "slice",
+        "split", "substring", "toLowerCase", "toLocaleLowerCase", "toUpperCase", "toLocaleUpperCase", "trim", "length", "substr", "valueOf",
+    ],
+    // TODO: GH#24690
+    isNewIdentifierLocation: true,
+});
